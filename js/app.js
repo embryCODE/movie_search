fbdb17f1d56f95c9aff3c663fe614464af37ed7d
Mason Embry <mason@embrycode.com>
2016-10-13 09:34:15 -0700

    Setup app.js.

---
+++ js/app.js
@@ -0,0 +1,21 @@
+(function($) {
+
+    'use strict';
+
+    /* Movie Search
+
+    Author: Mason Embry
+    Created 10/13/2016
+    Last Updated: 10/13/2016
+
+    Tested in current versions of Chrome, Safari, and Firefox.
+
+    */
+
+    
+
+
+
+
+
+})(jQuery);
